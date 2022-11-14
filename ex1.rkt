6276683f8597750950d9c89297c32e1af016db82
Raz Elbaz <93310416+RazElbaz@users.noreply.github.com>
2022-11-14 12:29:59 +0200

    Completion of task 1

--- ex1.rkt
+++ ex1.rkt
@@ -21,6 +21,7 @@ Problems I encountered: closing parentheses, due to the fact that only in the fi
   (cond [(null? lst) null]
         [else (append (first lst) (open-list (rest lst)))]
   ))
+(test (open-list null) =>  null)
 (test (open-list '(() () () ())) => '())
 (test (open-list '((1) (2) (3))) => '(1 2 3))
 (test (open-list '(() (1) () (2) () (3))) => '(1 2 3))
@@ -32,7 +33,7 @@ Task 1.2:
 In task 1.2 we were asked to get a list of lists of number types and to return a list that includes 2 numbers that contains the maximum and minimum in all lists.
 We used the function from Task 1.1 to merge all the lists and find the requested numbers within the merged list.
 
-We created 2 first helper function:
+First we created 2 helper functions:
 minimum Gets a list of numbers and returns the minimum number among them
 maximum: Gets a list of numbers and returns the maximum number among them
 
@@ -47,17 +48,18 @@ Problems I encountered: -
   (cond [(null? lst) +inf.0]
         [else (min (first lst) (minimum (rest lst)))]
   ))
+(test (minimum null) =>  +inf.0)
 (test (minimum '(1 2 3 4)) => 1.0)
 (test (minimum '(1/2 1/4)) => 0.25)
 (test (minimum '(1/2 2 3 4)) => 0.5)
 (test (minimum '(1 0.5 300 -10)) => -10.0)
 
-
 (: maximum : (Listof Number) -> Number)
 (define (maximum lst)
-  (cond [(null? lst) -inf.0]
+  (cond [(null? lst) -inf.0] ;Using Hint: You can use the default min/max values Values like +inf.0 / -inf.0 which refers to ±∞
         [else (max (first lst) (maximum (rest lst)))]
   ))
+(test (maximum null) =>  -inf.0)
 (test (maximum '(1 2 3 4)) => 4.0)
 (test (maximum '(1/2 1/4)) => 0.5)
 (test (maximum '(1/2 2 3 4)) => 4.0)
@@ -68,11 +70,40 @@ Problems I encountered: -
   (cond [(null? lst) null]
         [else (list(minimum(open-list lst)) (maximum(open-list lst)))]
   ))
-
+(test (min&max null) => null)
 (test (min&max '((1) (1) (1))) => '(1.0 1.0))       ;because of all the numbers are the same the min and the max should be the same number
 (test (min&max '((1) (2) (3))) => '(1.0 3.0))       ;a test that includes an empty list
+(test (min&max '(() ())) => '(+inf.0 -inf.0))
 (test (min&max '(() (1) () ())) => '(1.0 1.0))
 (test (min&max '(() () () ())) => '(+inf.0 -inf.0)) ;for testing the auxiliary functions
 (test (min&max '(() (1) () (2) () (3))) => '(1.0 3.0))
 (test (min&max '((1 1) (1) () (2) () (3))) => '(1.0 3.0))
 (test (min&max '((1 2 3) (2 3 3 4) (9 2 -1) (233 11 90))) => '(-1.0 233.0)) ;I took this test from the instructions of the assignment
+
+#|
+Task 1.3:
+In task 1.3 we were asked to perform the same task as 1.2 only using the apply function of Racket
+--Note: The helper functions we created in task 1.2 are not needed--
+
+Input: list of lists of type Number
+Output: A list that includes 2 numbers: maximum and minimum - from the union of all lists
+Problems I encountered: Lack of familiarity with the apply function - I learned and understood how to use it
+|#
+
+(:  min&max_apply : (Listof(Listof Number)) -> (Listof Number))
+(define ( min&max_apply lst)
+  (cond [(null? lst) null]
+        [(null? (open-list lst)) null]
+        [else (list(apply min(open-list lst)) (apply max(open-list lst)))]
+  ))
+(test (min&max_apply null) => null) ;check the first condition
+(test (min&max_apply '(() ())) => null) ;check the second condition
+(test (min&max_apply '((1))) => '(1 1))
+(test (min&max_apply '((1.0))) => '(1.0 1.0)) ;If one number is of type FLOAT then the new list should return with numbers of that type
+(test (min&max_apply '((1) (1) (1))) => '(1 1)) ;because of all the numbers are the same the min and the max should be the same number
+(test (min&max_apply '((1) (1) (1.0))) => '(1.0 1.0)) ;If one number is of type FLOAT then the new list should return with numbers of that type
+(test (min&max_apply '((1) (2) (3))) => '(1 3)) ;a test that includes an empty list
+(test (min&max_apply '(() (1) () ())) => '(1 1))
+(test (min&max_apply '(() (1) () (2) () (3))) => '(1 3))
+(test (min&max_apply '((1 1) (1) () (2) () (3))) => '(1 3))
+(test (min&max_apply '((1 2 3) (2 3 3 4) (9 2 -1) (233 11 90))) => '(-1 233)) ;I took this test from the instructions of the assignment
